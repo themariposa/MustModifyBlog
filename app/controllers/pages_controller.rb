05ed607425454af3351c45c2f4235cf6d408f2d3
Johnathon Wright <jw@mustmodify.com>
2020-01-29 17:04:51 -0500

    Implement portfolio page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def welcome
     render layout: 'wide'
   end
+
+  def portfolio
+    render layout: 'wide'
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,17 @@
 module ApplicationHelper
 
+  def page_title(value = nil)
+    if( value )
+      @page_title = value
+    else
+      @page_title
+    end
+  end
+
+  def page_title=(value)
+    @page_title = value
+  end
+
   def post_permalink_path(post, atts = {})
     datetime_part = post.created_at.strftime("%Y/%m/%d")
     "#{datetime_part}/#{post.slug}"
