d61d8d9c1639ac728636c84b5ae160f4760de535
Johnathon Wright <jw@mustmodify.com>
2020-01-29 17:07:15 -0500

    new seed process

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+file_names = Dir.glob('./db/old_content/*.md').sort
+file_names.each do |fn|
+  puts "====== [   #{ fn } ] ========================"
+  content = File.read(fn).split("---\n")
+  meta = YAML.load(content[1])
+
+  published = Date.parse(meta['published'])
+  slug = (meta['permalink'] && meta['permalink'].split('/').last) || meta['title'].to_slug
+  markdown = content[2..-1].join("---\n")
+  markdown = markdown.gsub("---", "```").gsub("&quot;", '"').gsub(/\nh1. /, "\n# ").gsub(/\nh1. /, "\n# ").gsub(/\nh3./, '###').gsub(/h2. /, "\n## ").gsub("&lt;", "<").gsub("&gt;", ">").gsub("&amp;", "&"),
+
+  Post.create(
+    published: !meta['draft'],
+    title: meta['title'],
+    markdown_content: markdown,
+    category: '',
+    slug: slug,
+    created_at: published
+  )
+end
