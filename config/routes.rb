fa8219c398d3f1404a71b52fa9883c4adefa0963
Johnathon Wright <jw@mustmodify.com>
2020-02-28 12:59:16 -0500

    previous URL, /portfolio, should redirect to /projects

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,5 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/portfolio' => redirect('/projects')
 end
